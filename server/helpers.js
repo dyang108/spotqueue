167c5e7b6baad326849b1803473e5d6035cd4c40
dyang108 <derickwyang@gmail.com>
2017-07-24 19:43:24 -0400

    remove server files

--- server/helpers.js
+++
@@ -1,9 +0,0 @@
-var _ = require('lodash')
-
-function pickTrackProps (trackRes) {
-  return _.pick(trackRes.body, ['id', 'name', 'popularity', 'artist', 'album', 'album', 'duration_ms'])
-}
-
-module.exports = {
-  pickTrackProps
-}
